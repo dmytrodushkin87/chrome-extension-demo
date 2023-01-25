09aa928cec5d0f8ef9db145711afa8058ca05100
dmytrodushkin87 <dmytrodushkin87@gmail.com>
2023-01-25 09:51:32 +0200

    'title': added script added the manifiest

---
+++ script.js
@@ -0,0 +1,9 @@
+async function fetchData() {
+    const res=await fetch ("https://api.coronavirus.data.gov.uk/v1/data");
+    const record=await res.json();
+    document.getElementById("date").innerHTML=record.data[0].date;
+    document.getElementById("areaName").innerHTML=record.data[0].areaName;
+    document.getElementById("latestBy").innerHTML=record.data[0].latestBy;
+    document.getElementById("deathNew").innerHTML=record.data[0].deathNew;
+}
+fetchData();
